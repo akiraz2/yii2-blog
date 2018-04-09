0c949136d8260935a742badd938878f64dee820b
akiraz2 <akiraz@bk.ru>
2018-04-09 23:16:15 +0300

    fix ModuleTraits, BlogPost List Widget

---
+++ widgets/PostList.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Project: yii2-blog for internal using
+ * Author: akiraz2
+ * Copyright (c) 2018.
+ */
+
+namespace akiraz2\blog\widgets;
+
+use akiraz2\blog\models\BlogPost;
+use akiraz2\blog\models\Status;
+use akiraz2\blog\Module;
+use akiraz2\blog\traits\IActiveStatus;
+use yii\base\Widget;
+use yii\db\Expression;
+
+class PostList extends Widget
+{
+    const TYPE_RANDOM = 'random';
+    const TYPE_POPULAR = 'hot';
+    const TYPE_RECENT = 'recent';
+
+    public $title;
+    public $maxPosts = 6;
+    public $type;
+
+    public function init()
+    {
+        parent::init();
+
+        if ($this->title === null) {
+            $this->title = Module::t('blog','Blog Posts');
+        }
+    }
+
+    public function run()
+    {
+        $posts = BlogPost::find()->where(['status' => IActiveStatus::STATUS_ACTIVE])->orderBy($this->getOrderFromType())->limit($this->maxPosts)->all();
+
+        return $this->render('post-list', [
+            'title' => $this->title,
+            'posts' => $posts,
+        ]);
+    }
+
+    protected function getOrderFromType() {
+        switch ($this->type) {
+            case self::TYPE_RANDOM: return new Expression('rand()');
+            case self::TYPE_RECENT: return ['created_at' => SORT_DESC];
+            case self::TYPE_POPULAR: return ['click' => SORT_DESC];
+            default: return [];
+        }
+    }
+}
---
+++ widgets/views/post-list.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Project: yii2-blog for internal using
+ * Author: akiraz2
+ * Copyright (c) 2018.
+ */
+
+use yii\helpers\Html;
+
+?>
+<section class="blog-list-widget">
+    <div class="blog-list-widget__header">
+        <h3 class="title title--3 blog-list-widget__title"><?= $title ?></h3>
+    </div>
+    <div class="blog-list-widget__content">
+        <?php foreach ($posts as $post): ?>
+            <div class="blog-list-widget__item blog-item">
+                <div class="blog-item__img">
+                    <?php echo Html::a(Html::img($post->getThumbFileUrl('banner', 'thumb'),['alt' => $post->title ]), $post->getUrl(), ['title' => $post->title]); ?>
+                </div>
+                <div class="blog-item__right">
+                    <?php echo Html::a(Html::encode($post->title), $post->getUrl(), ['class' => 'blog-item__url']); ?>
+                    <div class="blog-item__brief">
+                        <?= \yii\helpers\StringHelper::truncate($post->brief, 45);?>
+                    </div>
+                </div>
+            </div>
+        <?php endforeach; ?>
+    </div>
+</section>
